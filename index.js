71fbfe76823d6f86cca8607818ccf99f9bdcc97a
RaW <akram.iiuc.ctg@gmail.com>
2023-05-19 17:16:35 +0600

    galleryDB added

--- index.js
+++ index.js
@@ -8,6 +8,42 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+// ToyGalaxy
+// TdvaTcXd3wtY3VjX
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ToyGalaxy:TdvaTcXd3wtY3VjX@cluster0.6nxonq0.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("toyGalaxy");
+    const galleryCollection = database.collection("gallery");
+
+    // Gallery Collection 
+    app.get('/gallery', async(req,res) =>{
+        const result = await galleryCollection.find().toArray()
+        res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req,res) =>{
     res.send("Server is running")
